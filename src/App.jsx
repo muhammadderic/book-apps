6adc36a93c6e16e6f0e746e6030c7949a23b7ecb
muhammadderic <muhammad.donny.e@gmail.com>
2023-02-22 15:06:22 +0700

    Change theme's app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import { Route, Routes } from "react-router-dom"
 import Create from "./pages/Create"
 import Notes from "./pages/Notes"
 import Layout from "./components/Layout"
+import { createTheme, ThemeProvider } from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#316B83"
+    }
+  }
+})
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Notes />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
   )
 }
 
